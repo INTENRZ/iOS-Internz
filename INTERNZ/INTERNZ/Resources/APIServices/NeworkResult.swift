34af209d27b00b1eb7ecd34f6c2919f50c2aede6
Choieunji0114 <cheunji8209@naver.com>
2019-12-24 03:27:53 +0900

    Create APIServices

---
+++ INTERNZ/INTERNZ/Resources/APIServices/NeworkResult.swift
@@ -0,0 +1,24 @@
+//
+//  NeworkResult.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 24/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import Foundation
+
+// 네트워크 상태의 5가지 결과 ( 성공, 요청 에러, 경로 에러, 서버 에러, 통신 실패)
+enum NetworkResult<T> {
+    case success(T)
+    case requestErr(T)
+    case pathErr
+    case serverErr
+    case networkFail
+}
+
+struct APIConstants {
+    
+    static let BaseURL = ""
+    
+}
