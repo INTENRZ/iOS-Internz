afa8f69c77370f805d0da28fc35ec3c1b7ad5161
Choieunji0114 <cheunji8209@naver.com>
2019-12-24 02:33:56 +0900

    Create Tabbar ViewControllers

---
+++ INTERNZ/INTERNZ/Sources/ViewControllers/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 24/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.title = "홈"
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
