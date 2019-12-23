58c0169df134cb8d757ed637af283490f9196d49
Choieunji0114 <cheunji8209@naver.com>
2019-12-24 03:24:06 +0900

    Create UIColor+Additions

---
+++ INTERNZ/INTERNZ/Sources/Extensions/UIColor+Additions.swift
@@ -0,0 +1,125 @@
+//
+//  UIColor+Additions.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 24/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var veryLightPink: UIColor {
+    return UIColor(white: 232.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var marigold: UIColor {
+    return UIColor(red: 1.0, green: 194.0 / 255.0, blue: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white0: UIColor {
+    return UIColor(white: 1.0, alpha: 0.0)
+  }
+
+  @nonobjc class var veryLightPinkTwo: UIColor {
+    return UIColor(white: 227.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var darkGreyTwo: UIColor {
+    return UIColor(red: 33.0 / 255.0, green: 37.0 / 255.0, blue: 41.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGrey: UIColor {
+    return UIColor(white: 153.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var shamrock: UIColor {
+    return UIColor(red: 3.0 / 255.0, green: 180.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkThree: UIColor {
+    return UIColor(white: 226.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var darkGrey: UIColor {
+    return UIColor(red: 43.0 / 255.0, green: 45.0 / 255.0, blue: 47.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var black: UIColor {
+    return UIColor(white: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var paleGrey: UIColor {
+    return UIColor(red: 244.0 / 255.0, green: 244.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkFour: UIColor {
+    return UIColor(white: 183.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownishGrey: UIColor {
+    return UIColor(white: 102.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownishGreyTwo: UIColor {
+    return UIColor(white: 112.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var orangeyYellow: UIColor {
+    return UIColor(red: 245.0 / 255.0, green: 189.0 / 255.0, blue: 29.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var paleGreyTwo: UIColor {
+    return UIColor(red: 250.0 / 255.0, green: 250.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var marigoldTwo: UIColor {
+    return UIColor(red: 1.0, green: 188.0 / 255.0, blue: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var purpleishBlue: UIColor {
+    return UIColor(red: 81.0 / 255.0, green: 59.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var softBlue: UIColor {
+    return UIColor(red: 92.0 / 255.0, green: 161.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var candyPink: UIColor {
+    return UIColor(red: 1.0, green: 96.0 / 255.0, blue: 225.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteTwo: UIColor {
+    return UIColor(white: 244.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteThree: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var slateGrey: UIColor {
+    return UIColor(red: 101.0 / 255.0, green: 103.0 / 255.0, blue: 105.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownishGreyThree: UIColor {
+    return UIColor(white: 118.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkFive: UIColor {
+    return UIColor(white: 206.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkSix: UIColor {
+    return UIColor(white: 219.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGreyTwo: UIColor {
+    return UIColor(white: 173.0 / 255.0, alpha: 1.0)
+  }
+
+}
