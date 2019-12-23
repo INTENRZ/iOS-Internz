41b583df976ac7981f7127a25b5e63636c44c0db
Choieunji0114 <cheunji8209@naver.com>
2019-12-23 23:55:00 +0900

    Upload INTERNZ

---
+++ INTERNZ/INTERNZ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 23/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
