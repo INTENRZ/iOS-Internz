f6387451a4d59a854307b79667a1cd0c2e7a95ca
juyeonblue <jyp0211@ajou.ac.kr>
2019-12-25 15:48:27 +0900

    Create  Login storyboard

---
+++ INTERNZ/INTERNZ/Sources/ViewControllers/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  INTERNZ
+//
+//  Created by 박주연 on 25/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
