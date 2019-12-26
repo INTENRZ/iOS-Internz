0b8d7b13832b31659aec8637049858313cf1fef1
Choieunji0114 <cheunji8209@naver.com>
2019-12-27 06:46:50 +0900

    Create Signin storyboard

---
+++ INTERNZ/INTERNZ/Sources/ViewControllers/Signin/SigninViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SigninViewController.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 27/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "회원가입"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
