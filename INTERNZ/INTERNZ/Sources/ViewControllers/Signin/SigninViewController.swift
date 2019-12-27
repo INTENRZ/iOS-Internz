067743aa4426c21aef56402c1d870af22c8af3af
Choieunji0114 <cheunji8209@naver.com>
2019-12-27 17:41:31 +0900

    Update Signin

--- INTERNZ/INTERNZ/Sources/ViewControllers/Signin/SigninViewController.swift
+++ INTERNZ/INTERNZ/Sources/ViewControllers/Signin/SigninViewController.swift
@@ -9,24 +9,74 @@
 import UIKit
 
 class SigninViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var wholeView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "회원가입"
-
-        // Do any additional setup after loading the view.
+        
+        addKeyboardObserver()
+        
+        
+        emailTextField.delegate = self
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension SigninViewController: UITextFieldDelegate{
+    
+}
+
+extension SigninViewController {
+    // 옵저버 패턴 활용 TextField 가리는 문제 해결
+    private func addKeyboardObserver() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    private func closeKeyboardObserver(){
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+//            self.emailLabel.transform = .init(translationX: 0, y: -50)
+            self.wholeView.transform = .init(translationX: 0, y: -50)
+        })
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        UIView.animate(withDuration: duration, delay: 0, options: .init(rawValue: curve), animations: {
+            self.wholeView.transform = .identity
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
