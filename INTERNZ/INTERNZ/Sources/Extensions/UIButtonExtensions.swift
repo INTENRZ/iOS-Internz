56435f00ebd4f96ea0227d8430a4275049198547
Choieunji0114 <cheunji8209@naver.com>
2019-12-28 01:44:11 +0900

    Create Favor buttons + extensions

---
+++ INTERNZ/INTERNZ/Sources/Extensions/UIButtonExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIButtonExtensions.swift
+//  INTERNZ
+//
+//  Created by 최은지 on 28/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func setFavor() {
+
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.borderColor = UIColor.marigold.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 15
+        self.tintColor = UIColor.darkGreyTwo
+    
+    }
+}
+
--- INTERNZ/INTERNZ/Sources/ViewControllers/FirstLogin/SettingFavorViewController.swift
+++ INTERNZ/INTERNZ/Sources/ViewControllers/FirstLogin/SettingFavorViewController.swift
@@ -12,6 +12,39 @@ class SettingFavorViewController: UIViewController {
     
     @IBOutlet weak var completeButton: UIButton!
     
+    
+    @IBOutlet weak var gihwaekBtn: UIButton!
+    @IBOutlet weak var insaBtn: UIButton!
+    @IBOutlet weak var hwaegyeBtn: UIButton!
+    @IBOutlet weak var gyoyukBtn: UIButton!
+    @IBOutlet weak var gumaeBtn: UIButton!
+    @IBOutlet weak var hongboBtn: UIButton!
+    @IBOutlet weak var gwangoBtn: UIButton!
+    @IBOutlet weak var guknaeBtn: UIButton!
+    @IBOutlet weak var haewaeBtn: UIButton!
+    @IBOutlet weak var youngupBtn: UIButton!
+    @IBOutlet weak var marketingBtn: UIButton!
+    @IBOutlet weak var serviceBtn: UIButton!
+    
+    @IBOutlet weak var sangpumBtn: UIButton!
+    @IBOutlet weak var gongjeongBtn: UIButton!
+    @IBOutlet weak var saengsanBtn: UIButton!
+    @IBOutlet weak var pumzilBtn: UIButton!
+    
+    @IBOutlet weak var youtongBtn: UIButton!
+    @IBOutlet weak var yeonguBtn: UIButton!
+    @IBOutlet weak var designBtn: UIButton!
+    @IBOutlet weak var muyeokBtn: UIButton!
+    
+    @IBOutlet weak var itBtn: UIButton!
+    @IBOutlet weak var gongmuoneBtn: UIButton!
+    @IBOutlet weak var researchBtn: UIButton!
+    @IBOutlet weak var consultingBtn: UIButton!
+    @IBOutlet weak var guitarBtn: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,11 +52,46 @@ class SettingFavorViewController: UIViewController {
         
         self.completeButton.layer.cornerRadius = 5
         
+        
+        initBtn()
+        
 
         
     }
     
-
+    
+    // 버튼 초기 상태 setting
+    func initBtn(){
+        
+        gihwaekBtn.setFavor()
+        insaBtn.setFavor()
+        hwaegyeBtn.setFavor()
+        gyoyukBtn.setFavor()
+        gumaeBtn.setFavor()
+        hongboBtn.setFavor()
+        gwangoBtn.setFavor()
+        guknaeBtn.setFavor()
+        haewaeBtn.setFavor()
+        youngupBtn.setFavor()
+        marketingBtn.setFavor()
+        serviceBtn.setFavor()
+        sangpumBtn.setFavor()
+        gongjeongBtn.setFavor()
+        saengsanBtn.setFavor()
+        pumzilBtn.setFavor()
+        youtongBtn.setFavor()
+        yeonguBtn.setFavor()
+        designBtn.setFavor()
+        muyeokBtn.setFavor()
+        itBtn.setFavor()
+        gongmuoneBtn.setFavor()
+        researchBtn.setFavor()
+        consultingBtn.setFavor()
+        guitarBtn.setFavor()
+        
+        
+    }
     
 
 }
+
