521c41f2bc19e9ad8dd89e4234429bc43cf00bc7
juyeonblue <jyp0211@ajou.ac.kr>
2019-12-29 07:31:18 +0900

    Create Message StoryBoard

---
+++ INTERNZ/INTERNZ/Sources/Models/MessageUser.swift
@@ -0,0 +1,9 @@
+//
+//  MessageUser.swift
+//  INTERNZ
+//
+//  Created by 박주연 on 29/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import Foundation
---
+++ INTERNZ/INTERNZ/Sources/ViewControllers/Message/MessageProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MessageProfileViewController.swift
+//  INTERNZ
+//
+//  Created by 박주연 on 29/12/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class MessageProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
