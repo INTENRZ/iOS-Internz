7df0d9ce889212928bbf4a1a1a615db672be03ca
juyeonblue <jyp0211@ajou.ac.kr>
2019-12-29 06:37:10 +0900

    Update FollowTableCell

--- INTERNZ/INTERNZ/Sources/ViewControllers/ProfileTab/FollowTableViewCell.swift
+++ INTERNZ/INTERNZ/Sources/ViewControllers/ProfileTab/FollowTableViewCell.swift
@@ -15,18 +15,42 @@ class FollowTableViewCell: UITableViewCell {
     @IBOutlet weak var followButton: UIButton!
     @IBOutlet weak var followcontentLabel: UILabel!
     
+    var btn : Bool = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
         followButton.setCornerRadius()
         
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
     }
-
+    
+    // 버튼을 누르면 색과 안의 텍스트가 바뀐다.
+    @IBAction func startFollow(_ sender: UIButton) {
+        
+        if btn == false{
+            
+            sender.layer.backgroundColor = UIColor.marigold.cgColor
+            followButton.setTitle("팔로우", for: .normal)
+            btn = true
+        }
+            
+        else{
+            
+            sender.layer.backgroundColor = UIColor.whiteFour.cgColor
+            followButton.setTitle("팔로잉", for: .normal)
+            btn = false
+        }
+        
+        
+    }
+    
+    
+    
 }
 
 
