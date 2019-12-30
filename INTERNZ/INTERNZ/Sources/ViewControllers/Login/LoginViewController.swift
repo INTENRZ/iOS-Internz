d03ef96a58620a86153e770a172a3b1e7b4123b2
Choieunji0114 <cheunji8209@naver.com>
2019-12-30 18:58:08 +0900

    Update LoginViewController.swift

--- INTERNZ/INTERNZ/Sources/ViewControllers/Login/LoginViewController.swift
+++ INTERNZ/INTERNZ/Sources/ViewControllers/Login/LoginViewController.swift
@@ -60,22 +60,39 @@ class LoginViewController: UIViewController {
 //                print(user_data)
                 
                 UserDefaults.standard.set(user_data.token, forKey: "token")
-                UserDefaults.standard.set(user_data.refreshToken, forKey: "refreshToken")
                 UserDefaults.standard.set(user_data.isFirst, forKey: "isFirst")
                 
-                let firstLoginStoryBoard:UIStoryboard = UIStoryboard(name: "FirstLogin", bundle: nil)
-
-                let dvc = firstLoginStoryBoard.instantiateViewController(identifier: "FirstLogin") as! SettingFavorViewController
-
-                let navigationController = UINavigationController(rootViewController: dvc)
-
-                navigationController.modalPresentationStyle = .fullScreen
-                self.present(navigationController, animated: true, completion: nil)
+                print("isFirst??")
+                print(UserDefaults.standard.string(forKey: "isFirst"))
+                
+                if(UserDefaults.standard.string(forKey: "isFirst") == "0"){
+                    print("처음 로그인")
+                    self.goFirstLogin()
+                } else {
+                    print("이미 로그인 해봄")
+                }
+                
+//                let firstLoginStoryBoard:UIStoryboard = UIStoryboard(name: "FirstLogin", bundle: nil)
+//
+//                let dvc = firstLoginStoryBoard.instantiateViewController(identifier: "FirstLogin") as! SettingFavorViewController
+//
+//                let navigationController = UINavigationController(rootViewController: dvc)
+//
+//                navigationController.modalPresentationStyle = .fullScreen
+//                self.present(navigationController, animated: true, completion: nil)
                 
                 
                 
             case .requestErr(let message) :
-                print(message)
+//                print(message)
+//                print("적절하지 않은 로그인")
+                
+               let alert = UIAlertController(title: "Login Failed!", message: "이메일과 비밀번호를 다시 확인해 주세요", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+
+               self.present(alert, animated: true)
+                
+                
                 break
                 
             case .pathErr:
@@ -90,13 +107,29 @@ class LoginViewController: UIViewController {
                 
             }
         }
-    
+        
         
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    func goMain(){
+        
+    }
+    
+    
+    func goFirstLogin(){
+        let firstLoginStoryBoard:UIStoryboard = UIStoryboard(name: "FirstLogin", bundle: nil)
+
+        let dvc = firstLoginStoryBoard.instantiateViewController(identifier: "FirstLogin") as! SettingFavorViewController
+
+        let navigationController = UINavigationController(rootViewController: dvc)
+
+        navigationController.modalPresentationStyle = .fullScreen
+        self.present(navigationController, animated: true, completion: nil)
     }
     
 }
@@ -124,7 +157,7 @@ extension LoginViewController: UITextFieldDelegate {
     }
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        print("did change selection")
+//        print("did change selection")
     }
 }
 
